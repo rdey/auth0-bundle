738e119b5da7725fd7e0317cfd8567a3c3da6167
Magnus Nordlander <magnus@nordlander.se>
2017-11-21 10:28:23 +0100

    Added JWT leeway

--- Model/Authentication/Claims.php
+++ Model/Authentication/Claims.php
@@ -17,13 +17,15 @@ class Claims
      */
     public function __construct(array $claims)
     {
-        dump($claims);
         $this->claims = $claims;
     }
 
     public static function createFromJWT(string $jwt, string $key)
     {
+        $previousLeeway = JWT::$leeway;
+        JWT::$leeway = 5;
         $claims = JWT::decode($jwt, $key, ['HS256', 'HS384', 'HS512', 'RS256']);
+        JWT::$leeway = $previousLeeway;
         return new static((array)$claims);
     }
 
